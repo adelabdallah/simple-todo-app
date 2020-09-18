2fd4907211b8d64d23e585896039ad4ce32fcdff
Adel <adeloabdallah1995@gmail.com>
2020-09-18 12:27:10 -0400

    Basic project skeleton set up

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        loader: 'babel-loader',
+        // if file getting loaded ends with .js
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        // if file ends in .css or .scss
+        test: /\.s?css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
+  }
+};
